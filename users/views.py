4053c03dbfdceac295270014581bbee67d7aead3
79244 <dumichkira05@gmail.com>
2024-05-23 09:48:08 +1000

    Создание отображений для пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import generics, status
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from materials.models import Course
+from users.models import User
+from users.serializers import UserRegisterSerializer, UserSerializer
+
+
+class UserRegisterView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        password = serializer.data['password']
+        user = User.objects.get(pk=serializer.data['id'])
+        user.set_password(password)
+        user.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class UserRetrieveView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserListView(generics.ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserDeleteView(generics.DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
