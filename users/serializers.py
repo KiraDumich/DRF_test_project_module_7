ca26a1ba669720d3bdb5f96c5dc42eb025f55f0f
79244 <dumichkira05@gmail.com>
2024-05-24 21:28:06 +1000

    Доработки

--- users/serializers.py
+++ users/serializers.py
@@ -11,16 +11,24 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-    """Класс сериализатора для регистрации пользователя"""
-
-    class Meta:
-        model = User
-        fields = ['email', 'password']
-
-
 class PaymentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Payment
         fields = '__all__'
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create(
+          email=validated_data['email'],
+          password=validated_data['password']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
