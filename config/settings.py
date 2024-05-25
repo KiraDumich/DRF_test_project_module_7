593705742b4d34c23884b67baaeaca0a9c581096
79244 <dumichkira05@gmail.com>
2024-05-25 12:54:05 +1000

    Исправление настроек

--- config/settings.py
+++ config/settings.py
@@ -126,9 +126,9 @@ REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
        'rest_framework.authentication.TokenAuthentication',
    ),
-   'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
-   ),
+   # 'DEFAULT_PERMISSION_CLASSES': (
+   #      'rest_framework.permissions.IsAdminUser',
+   # ),
 }
 
 AUTH_USER_MODEL = 'users.User'
