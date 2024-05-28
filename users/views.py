e695cc0af56a9628c6fdb522cf68c51b29dc8220
79244 <dumichkira05@gmail.com>
2024-05-28 14:35:40 +1000

    #24.2

--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
-from django_filters import filters
-from rest_framework import generics, status
 
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 from materials.models import Course
 from users.models import User, Payment
 from users.serializers import UserSerializer, CreateUserSerializer, PaymentSerializer
@@ -38,7 +38,6 @@ class UserDeleteView(generics.DestroyAPIView):
 class PaymentViewSet(ModelViewSet):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
-
-    filter_backends = [filters.OrderingFilter]
-    filterset_fields = ('paid_course', 'paid_lesson')
-    ordering_fields = ('payments_date',)
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method')
+    ordering_fields = ('payment_date',)
